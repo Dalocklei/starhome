0c7cda91396a20449caa4eb196b61a4ab836193e
Dalocklei <lei.jia08@gmail.com>
2015-04-23 23:31:24 +0800

    add the backend part

---
+++ backend/findStar.php
@@ -0,0 +1,15 @@
+<?php
+/**
+	匹配球星结果的函数库,共5个函数，参数需要调整
+	findStar_1($height, $weight, $speed, $bounce)    
+	findStar_2($height, $weight, $speed, $bounce)
+	findStar_3($height, $weight, $speed, $bounce)
+	findStar_4($height, $weight, $speed, $bounce)
+	findStar_5($height, $weight, $speed, $bounce)
+*/
+
+	function findStar_1($height, $weight, $speed, $bounce)
+	{
+		
+	}
+?>
